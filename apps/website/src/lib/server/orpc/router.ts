fc3cf7aae505ca0d1d896d4120fdf3af9af4745a
Oung Seik <mhemaungthuwin@gmail.com>
2026-03-08 14:43:45 +0630

    feat: implement API to list products

--- apps/website/src/lib/server/orpc/router.ts
+++ apps/website/src/lib/server/orpc/router.ts
@@ -2,13 +2,19 @@ import { createRouterClient } from "@orpc/server";
 
 import { os } from "./base";
 import { uploadImageHandler } from "./handlers/images/upload_image";
+import { listProductsHandler } from "./handlers/products/list_products";
 import { createShopHandler } from "./handlers/shops/create_shop";
 import { getShopHandler } from "./handlers/shops/get_shop";
 import { updateShopHandler } from "./handlers/shops/update_shop";
 
 export const router = os.router({
   images: { uploadImage: uploadImageHandler },
-  shops: { createShop: createShopHandler, getShop: getShopHandler, updateShop: updateShopHandler },
+  products: { list: listProductsHandler },
+  shops: {
+    createShop: createShopHandler,
+    getShop: getShopHandler,
+    updateShop: updateShopHandler,
+  },
 });
 
 /** only for server-side call */
