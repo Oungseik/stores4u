a68669c7e15ff5f738731fd9b9414d7cdf3d5aee
Oung Seik <mhemaungthuwin@gmail.com>
2026-03-14 12:08:43 +0630

    fix: DATABASE_URL is used by the primary db

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   out: "./migrations",
   dialect: "turso",
   dbCredentials: {
-    url: process.env.DATABASE_URL ?? process.env.PARENT_DATABASE_URL!,
+    url: process.env.PARENT_DATABASE_URL!,
     authToken: process.env.TURSO_GROUP_AUTH_TOKEN,
   },
 });
