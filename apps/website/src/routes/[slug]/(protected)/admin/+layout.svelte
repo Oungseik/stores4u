d2e71463851d4b29c390414cb4916718524c4caf
Oung Seik <mhemaungthuwin@gmail.com>
2026-03-16 09:30:35 +0630

    fix: show sidebar header to control sidebar in mobile view

--- apps/website/src/routes/[slug]/(protected)/admin/+layout.svelte
+++ apps/website/src/routes/[slug]/(protected)/admin/+layout.svelte
@@ -25,15 +25,15 @@
   >
     <AdminSidebar variant="inset" {shop} {user} {currentPath} />
     <Sidebar.Inset>
-      <!-- <header -->
-      <!--   class="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear" -->
-      <!-- > -->
-      <!--   <div class="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6"> -->
-      <!--     <Sidebar.Trigger class="-ms-1" /> -->
-      <!--     <Separator orientation="vertical" class="mx-2 data-[orientation=vertical]:h-4" /> -->
-      <!--     <h1 class="text-base font-medium">Admin Dashboard</h1> -->
-      <!--   </div> -->
-      <!-- </header> -->
+      <header
+        class="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear"
+      >
+        <div class="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
+          <Sidebar.Trigger class="-ms-1" />
+          <Separator orientation="vertical" class="mx-2 data-[orientation=vertical]:h-4" />
+          <h1 class="text-base font-medium">Checkout</h1>
+        </div>
+      </header>
       <div class="flex flex-1 flex-col">
         {@render children?.()}
       </div>
