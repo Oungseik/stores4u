1b932b78dd2ad80ab6797fdaf80007c38d67986a
Oung Seik <mhemaungthuwin@gmail.com>
2026-03-19 20:02:56 +0630

    chore: prepare for build with opentelemetry

--- apps/website/svelte.config.js
+++ apps/website/svelte.config.js
@@ -1,6 +1,6 @@
-import adapter from "@sveltejs/adapter-auto";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 import { mdsvex } from "mdsvex";
+import adapter from "svelte-adapter-bun";
 
 const config = {
   preprocess: [vitePreprocess(), mdsvex()],
