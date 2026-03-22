2ac97c9fa11e12cdba55e93cbb38acf6baa1166b
Oung Seik <mhemaungthuwin@gmail.com>
2026-03-22 15:09:53 +0630

    chore: setup with bun instead of pnpm

--- flake.nix
+++ flake.nix
@@ -1,24 +1,114 @@
 {
+  description = "Stores For You - SvelteKit POS monorepo";
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    utils.url = "github:numtide/flake-utils";
+    flake-utils.url = "github:numtide/flake-utils";
   };
+
   outputs =
     {
       self,
       nixpkgs,
-      utils,
+      flake-utils,
     }:
-    utils.lib.eachDefaultSystem (
+    flake-utils.lib.eachSystem [ "x86_64-linux" ] (
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+
+        website =
+          let
+            srcFiles = builtins.path {
+              name = "stores4u-src";
+              path = ./.;
+              filter =
+                path: type:
+                let
+                  name = baseNameOf path;
+
+                  isGit = name == ".git";
+                  isNix = name == "nix" && type == "directory";
+                  isFlake = name == "flake.nix" || name == "flake.lock";
+                  isNodeModules = name == "node_modules";
+                  isSvelteKit = name == ".svelte-kit";
+                  isBuildArtifact = name == "dist" || name == ".turbo";
+                in
+                !(isGit || isNix || isFlake || isNodeModules || isSvelteKit || isBuildArtifact);
+            };
+
+            pnpmDeps = pkgs.pnpm.fetchDeps {
+              pname = "stores4u-website-deps";
+              version = "0.0.1";
+              src = srcFiles;
+              hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+              fetcherVersion = 1;
+            };
+          in
+          pkgs.stdenv.mkDerivation {
+            pname = "stores4u-website";
+            version = "0.0.1";
+
+            src = srcFiles;
+
+            inherit pnpmDeps;
+
+            nativeBuildInputs = with pkgs; [
+              pnpm
+              bun
+              nodejs
+              cacert
+              makeWrapper
+            ];
+
+            configurePhase = ''
+              runHook preConfigure
+
+              export HOME=$(mktemp -d)
+
+              pnpm config set manage-package-manager-versions false
+
+              runHook postConfigure
+            '';
+
+            buildPhase = ''
+              runHook preBuild
+
+              pnpm install --offline --frozen-lockfile --ignore-scripts
+
+              pnpm --filter website build
+
+              runHook postBuild
+            '';
+
+            installPhase = ''
+              runHook preInstall
+
+              mkdir -p $out/lib/website $out/bin
+
+              cp -r apps/website/.svelte-kit/adapter-bun/* $out/lib/website/
+
+              makeWrapper ${pkgs.bun}/bin/bun $out/bin/website \
+                --add-flags "run" \
+                --add-flags "$out/lib/website/index.js" \
+                --set-default PORT 3000
+
+              runHook postInstall
+            '';
+          };
       in
       {
-        devShell = pkgs.mkShell {
+        packages = {
+          inherit website;
+          default = self.packages.${system}.website;
+        };
+
+        devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
+            bun
+            pnpm
+            nodejs
             biome
-            otel-desktop-viewer
           ];
 
           LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib";
