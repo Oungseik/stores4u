210016d54ae084f01148333b6c2af8152cd1bcf8
Oung Seik <mhemaungthuwin@gmail.com>
2026-03-22 11:41:02 +0630

    chore: add build script for UI

---
+++ packages/ui/src/app.d.ts
@@ -0,0 +1,15 @@
+// see https://svelte.dev/docs/kit/types#app.d.ts
+// for information about these interfaces
+declare global {
+	namespace app {
+		// interface error {}
+		// interface locals {}
+		// interface pagedata {}
+		// interface pagestate {}
+		// interface platform {}
+	}
+}
+
+export {};
+
+
