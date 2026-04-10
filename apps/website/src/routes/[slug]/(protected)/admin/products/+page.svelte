a71bc90f239a6d97d630d59dff198fd548839e7d
Oung Seik <mhemaungthuwin@gmail.com>
2026-04-10 22:44:59 +0630

    fix: wrap the search bar

--- apps/website/src/routes/[slug]/(protected)/admin/products/+page.svelte
+++ apps/website/src/routes/[slug]/(protected)/admin/products/+page.svelte
@@ -188,7 +188,7 @@
   <section class="space-y-6">
     <!-- Filters and Search -->
     <FilterBar.Root {hasFilters} onReset={resetFilters} class="justify-between">
-      <div class="flex items-center justify-start gap-4">
+      <div class="flex flex-wrap items-center justify-start gap-2 md:gap-4">
         <FilterBar.Search
           placeholder="Search products, SKU..."
           value={searchParams.search}
--- apps/website/src/routes/[slug]/(protected)/admin/products/categories/+page.svelte
+++ apps/website/src/routes/[slug]/(protected)/admin/products/categories/+page.svelte
@@ -131,7 +131,7 @@
 
   <section class="space-y-6">
     <FilterBar.Root {hasFilters} onReset={resetFilters} class="justify-between">
-      <div class="flex items-center justify-start gap-4">
+      <div class="flex flex-wrap items-center justify-start gap-2 md:gap-4">
         <FilterBar.Search
           placeholder="Search categories..."
           value={searchParams.search}
