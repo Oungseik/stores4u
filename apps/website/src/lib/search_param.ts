d1958db50ba03cba2e57256c0b7efd7fc8d367db
Oung Seik <mhemaungthuwin@gmail.com>
2026-04-14 09:19:41 +0630

    chore: format the code

--- apps/website/src/lib/search_param.ts
+++ apps/website/src/lib/search_param.ts
@@ -1,8 +1,4 @@
-import type {
-  MovementType,
-  PurchaseInvoiceFileStatus,
-  PurchaseInvoiceStatus,
-} from "@repo/db";
+import type { MovementType, PurchaseInvoiceFileStatus, PurchaseInvoiceStatus } from "@repo/db";
 import { createSearchParamsSchema, type StandardSchemaV1 } from "runed/kit";
 
 type CheckoutMode = {
